17d669fed34b81723e274773480dc30e1a2a554b
OsimFavour <famos204@gmail.com>
2024-07-12 07:53:01 +0100

    Change styling for sign up button

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -103,7 +103,7 @@ const SignUpForm = () => {
                     value={confirmPassword}
                 />
 
-                <Button buttonType='google' type="submit">Sign Up</Button>                
+                <Button type="submit">Sign Up</Button>                
             </form>
         </div>
     )
