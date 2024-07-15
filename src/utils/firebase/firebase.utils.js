d12ab026f349048eea668d5fb31e7430b0fe5e19
OsimFavour <famos204@gmail.com>
2024-07-15 06:53:26 +0100

    Update onAuthStateChanged with an auth argument

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,7 @@ import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signOut,
+    onAuthStateChanged
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
@@ -79,4 +80,7 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 }
 
 
-export const signOutUser = async () => await signOut();
+export const signOutUser = async () => await signOut(auth);
+
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback)
