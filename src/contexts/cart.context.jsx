254997df9a391bb816ec2ef8faf2e38a0ca31d95
OsimFavour <famos204@gmail.com>
2024-07-19 18:44:35 +0100

    Update state handler

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -6,7 +6,7 @@ export const CartContext = createContext({
     cartItems: [],
     addItemsToCart: () => {}
 })
-g
+
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false)
 
