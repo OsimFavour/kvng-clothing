87b2e949a9eec890952fc83942e2553788b3a292
OsimFavour <famos204@gmail.com>
2024-07-20 17:01:28 +0100

    Pull off cart items from cart context

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
+
+import { CartContext } from '../../contexts/cart.context'
 
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
+
+    const { cartItems } = useContext(CartContext)
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                {[].map((item) => <CartItem cartItem={item}/>)}
+                {cartItems.map((item) => <CartItem cartItem={item}/>)}
             </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
