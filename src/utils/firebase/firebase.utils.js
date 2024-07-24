834e36fe6e3b801ecd8a787a69556dfc6910ee6e
OsimFavour <famos204@gmail.com>
2024-07-24 05:02:03 +0100

    Created new method to add new collection and document to firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { 
-    getAuth,
-    signInWithPopup,
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged
+  signOut,
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+import { 
+  doc, 
+  setDoc,
+  getDoc, 
+  collection,
+  writeBatch,
+  getFirestore, 
+} from 'firebase/firestore'
 
 
 const firebaseConfig = {
@@ -33,6 +41,19 @@ export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 
 export const db = getFirestore()
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey)
+  const batch = writeBatch(db)
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase())
+    batch.set(docRef, object)
+  })
+
+  await batch.commit()
+  console.log('done');
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
 
