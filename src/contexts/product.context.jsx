a0477b55ce080564f0d456f3f3900243ed449d77
OsimFavour <famos204@gmail.com>
2024-07-24 05:12:42 +0100

    Remove the method so as to avoid creating a new category after every mounting

--- src/contexts/product.context.jsx
+++ src/contexts/product.context.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import SHOP_DATA from '../shop-data.js'
 import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
@@ -9,12 +8,6 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
     const [products, setProducts] = useState([])
-
-    useEffect(() => {
-      addCollectionAndDocuments('categories', SHOP_DATA)
-    
-    }, [])
-
     
     const value = { products }
     return (
