7bf21e3d9fa6e27b88033729b58387bcb7c00341
OsimFavour <famos204@gmail.com>
2024-07-28 21:15:51 +0100

    Renamed Products Provider to Categories Provider

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/product.context';
 import { CartProvider } from './contexts/cart.context';
+import { CategoriesProvider } from './contexts/categories.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
