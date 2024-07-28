92ea703c67bc97a72666c8df8ddfe771c9a48a04
OsimFavour <famos204@gmail.com>
2024-07-28 21:10:52 +0100

    Create categories preview folder

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment, useContext } from 'react'
+import { CategoriesContext } from '../../contexts/categories.context'
+
+import CategoryPreview from '../../components/category-preview/category-preview.component'
+
+
+const CategoriesPreview = () => {
+
+    const { categoriesMap } = useContext(CategoriesContext)
+
+    console.log('Categories Map', categoriesMap);
+
+    return (
+        <div className='shop-container'>
+            {Object.keys(categoriesMap).map((title) => {
+                const products = categoriesMap[title]
+
+                return (
+                    <CategoryPreview key={title} title={title} products={products}/>   
+                )
+            })}
+        </div>
+    )
+}
+
+export default CategoriesPreview
