e03624309cfb65484fdc784a85642c01b8972741
OsimFavour <famos204@gmail.com>
2024-08-01 13:42:25 +0100

    Modify new branch

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,21 +1,20 @@
 import { useContext } from 'react'
-
+import './checkout.styles.scss'
 import { CartContext } from '../../contexts/cart.context'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
-import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from './checkout.styles'
 
 const Checkout = () => {
     const { cartItems, cartTotal } = useContext(CartContext) 
 
     return (
-        <CheckoutContainer>
-            <CheckoutHeader>
-                <HeaderBlock><span>Product</span></HeaderBlock>
-                <HeaderBlock><span>Description</span></HeaderBlock>
-                <HeaderBlock><span>Quantity</span></HeaderBlock>
-                <HeaderBlock><span>Price</span></HeaderBlock>
-                <HeaderBlock><span>Remove</span></HeaderBlock>
-            </CheckoutHeader>
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className="header-block"><span>Product</span></div>
+                <div className="header-block"><span>Description</span></div>
+                <div className="header-block"><span>Quantity</span></div>
+                <div className="header-block"><span>Price</span></div>
+                <div className="header-block"><span>Remove</span></div>
+            </div>
 
             
             {cartItems.map((cartItem) => (
@@ -23,9 +22,9 @@ const Checkout = () => {
             ))}
             
 
-            <Total>Total: ${cartTotal}</Total>
+            <span className="total">Total: ${cartTotal}</span>
         
-        </CheckoutContainer>
+        </div>
 
         
     )
