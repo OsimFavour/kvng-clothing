85add7673455fc884fa8cf57fc47ccd740749d84
OsimFavour <famos204@gmail.com>
2024-08-01 12:41:05 +0100

    Convert app to styled component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,16 +1,15 @@
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-
-import './authenticaton.styles.scss'
+import { AuthenticationContainer } from "./authenticaton.styles";
 
 const Authentication = () => {
 
     return (
-        <div className="authentication-container">
+        <AuthenticationContainer>
 
             <SignInForm/>
             <SignUpForm/>
-        </div>
+        </AuthenticationContainer>
     )
 }
 
