9cd1d41748827a66f97843c1cb4b3f5ba212fba8
OsimFavour <famos204@gmail.com>
2024-08-01 12:46:40 +0100

    Convert HTML elements and components to styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,23 +1,18 @@
-import './directory-item.styles.scss'
+import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles"
 
 const DirectoryItem = ({ category }) => {
 
     const { id, title, imageUrl } = category
 
     return (
-        <div key={id} className="directory-item-container">
-            <div 
-                className="background-image" 
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }} 
-            />
+        <DirectoryItemContainer key={id}>
+            <BackgroundImage imageUrl={imageUrl}/>
 
-            <div className="body">
+            <Body>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
-            </div>
-        </div>
+            </Body>
+        </DirectoryItemContainer>
     )
 }
 
