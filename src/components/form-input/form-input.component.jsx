e930de41c882689b5299737da6112f6632c5efc8
OsimFavour <famos204@gmail.com>
2024-08-01 12:44:01 +0100

    Convert app to styled component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,23 +1,19 @@
-import './form-input.styles.scss'
+import { FormInputLabel, Group } from './form-input.styles.jsx'
 
 const FormInput = ({ label, ...otherProps}) => {
     return (
-        <div className='group'>
+        <Group>
             
-            <input className='form-input' {...otherProps}/>
+            <FormInput {...otherProps}/>
             
             {label && (
-                <label 
-                    className={`${
-                        otherProps.value.length ? 'shrink' : ''
-                    } form-input-label`}
-                >
+                <FormInputLabel shrink={otherProps.value.length}>
                     {label}
-                </label>
+                </FormInputLabel>
 
             )}
 
-        </div>
+        </Group>
     )
 }
 
