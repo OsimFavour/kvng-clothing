f2593c2e191ed38d7eaedbe8dd2fd0411dfc37e2
OsimFavour <famos204@gmail.com>
2024-10-09 11:11:23 +0100

    Separate async logic from component with Redux Thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 
 import Category from '../category/category.component'
 import CategoriesPreview from '../categories-preview/categories-preview.component'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
-import { setCategories } from '../../store/categories/category.action'
+
+import { fetchCategoriesAsync } from '../../store/categories/category.action'
+
 
 const Shop = () => {
 
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments('categories')
-            console.log(categoriesArray);
-            
-            dispatch(setCategories(categoriesArray))
-        }
-
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync())
     }, [])
 
     return (
