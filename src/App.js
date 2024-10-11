83bf1d1152d966153d287552763ce10b106c3565
OsimFavour <famos204@gmail.com>
2024-10-11 09:38:03 +0100

    Remove code thats not needed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { useEffect } from 'react';
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
-import { setCurrentUser } from './store/user/user.action';
-import { useDispatch } from 'react-redux';
+
+import { onCheckUserSession } from './store/user/user.saga';
 
 
 const App = () => {
@@ -15,17 +16,7 @@ const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-
-        if (user) {
-            createUserDocumentFromAuth(user)
-        }
-
-        dispatch(setCurrentUser(user))
-        console.log('Unsubscribe>>>>>', user);
-      })
-
-      return unsubscribe
+    dispatch(onCheckUserSession())
   }, [])
 
   return (
