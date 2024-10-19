e7f6ec85118708b2673c0d89102dae51e017b864
OsimFavour <famos204@gmail.com>
2024-10-19 20:57:03 +0100

    Separate code concerns

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,14 +1,11 @@
 import { useState } from "react"
 
-import { 
-    createAuthUserWithEmailAndPassword, 
-    createUserDocumentFromAuth 
-} from "../../utils/firebase/firebase.utils"
-
 import FormInput from "../form-input/form-input.component"
 import Button from "../button/button.component"
 
 import { SignUpContainer } from './sign-up-form.styles'
+import { useDispatch } from "react-redux"
+import { signUpStart } from "../../store/user/user.action"
 
 
 const defaultFormFields = {
@@ -19,6 +16,8 @@ const defaultFormFields = {
 }
 
 const SignUpForm = () => {
+
+    const dispatch = useDispatch()
 
     const [formFields, setFormFields] = useState(defaultFormFields)
     const { displayName, email, password, confirmPassword } = formFields
@@ -38,14 +37,9 @@ const SignUpForm = () => {
         }
         
         try {
-            const { user } = await createAuthUserWithEmailAndPassword(
-                email, 
-                password
-            )
+            dispatch(signUpStart())
 
-            await createUserDocumentFromAuth(user, { displayName })
-
-            resetFormFields()
+            resetFormFields(email, password, displayName)
 
         } catch (error) {
             if (error.code === 'auth/network-request-failed') {
